6e1dd59353f812804764701658a2268277deef9b
Tamoha01181122 <78940879+Tamoha01181122@users.noreply.github.com>
2021-07-19 18:19:11 +0900

    Update libgeom.cpp

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -1,13 +1,13 @@
-﻿#include "pch.h"
-#include "../include/libgeom.h"
+//#include "pch.h"
+//#include "../include/libgeom.h"
 #include <algorithm>
 
 bool AABB::isCollide(const AABB& a, const AABB& b)
 {
 	
-	if(std::min(a.max_[0].b.max_[0])<std::max(a.min_[0].b.min_[0]))return false;
-	if(std::min(a.max_[1].b.max_[1])<std::max(a.min_[1].b.min_[1]))return false;
-	if(std::min(a.max_[2].b.max_[2])<std::max(a.min_[2].b.min_[2]))return false;
+	if(std::min(a.max_[0]. b.max_[0]) < std::max(a.min_[0]. b.min_[0])) return false;
+	if(std::min(a.max_[1]. b.max_[1]) < std::max(a.min_[1]. b.min_[1])) return false;
+	if(std::min(a.max_[2]. b.max_[2]) < std::max(a.min_[2]. b.min_[2])) return false;
 	
 	return true;
 }
