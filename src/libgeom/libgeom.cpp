4b2a739afad59b92576cbf1a04eccca584972b03
Tamoha01181122 <78940879+Tamoha01181122@users.noreply.github.com>
2021-07-19 16:21:18 +0900

    Update libgeom.cpp

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -1,18 +1,13 @@
 ﻿#include "pch.h"
 #include "../include/libgeom.h"
 
-
-Struct AABB{
-	Point min;
-	Point max;
-}
-
-
 bool AABB::isCollide(const AABB& a, const AABB& b)
 {
 	//if(a.max[0]<b.min[0]||b.max[0]<a.min[0])return false;
 	//if(a.man[1]<b.min[1]||b.max[1]<a.min[1])return false;
 	//if(a.max[2]<b.min[2]||b.max[2]<a.min[2])return false;
+	
+	
 	
 	return a.min_[0]<a.max_[1];
 }; 
