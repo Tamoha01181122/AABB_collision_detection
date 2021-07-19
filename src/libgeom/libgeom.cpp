de9500e4f240f5b37ed984eca04df62742ef860c
Tamoha01181122 <78940879+Tamoha01181122@users.noreply.github.com>
2021-07-19 17:16:18 +0900

    Update libgeom.cpp

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -5,6 +5,8 @@ bool AABB::isCollide(const AABB& a, const AABB& b)
 {
 	
 	if(a.min_[0]<a.max_[0]||b.min_[0]<b.max_[0])return false;
+	if(a.min_[1]<a.max_[1]||b.min_[1]<b.max_[1])return false;
+	
 	
 	return true;
 }
